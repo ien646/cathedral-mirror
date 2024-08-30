f96b26c52db30eff427d98bb5eba79a3d3b70217
ien <ien@amsostudios.com>
2024-08-30 00:00:00 +0000

    dxt1/dxt5 texture compression

---
+++ lib/engine/src/texture_compression.cpp
@@ -0,0 +1,60 @@
+#include <cathedral/engine/texture_compression.hpp>
+
+#include <cathedral/core.hpp>
+
+#include <ien/image/image.hpp>
+
+#include <stb_dxt.h>
+
+namespace cathedral::engine
+{
+    std::vector<uint8_t> compress_dxt1(const ien::image& img)
+    {
+        std::vector<uint8_t> result;
+        for(size_t y = 0; y < img.height(); y += 4)
+        {
+            for(size_t x = 0; x < img.width(); x += 4)
+            {
+                std::array<uint8_t, 8> block_data;
+                std::fill(block_data.begin(), block_data.end(), 0);
+                const auto image_block = img.copy_rect(x, y, 4, 4);
+                stb_compress_dxt_block(block_data.data(), image_block.data(), 0, STB_DXT_HIGHQUAL);
+                std::copy(block_data.begin(), block_data.end(), std::back_inserter(result));
+            }
+        }
+        return result;
+    }
+
+    std::vector<uint8_t> compress_dxt5(const ien::image& img)
+    {
+        std::vector<uint8_t> result;
+        for(size_t y = 0; y < img.height(); y += 4)
+        {
+            for(size_t x = 0; x < img.width(); x += 4)
+            {
+                std::array<uint8_t, 16> block_data;
+                std::fill(block_data.begin(), block_data.end(), 0);
+                const auto image_block = img.copy_rect(x, y, 4, 4);
+                stb_compress_dxt_block(block_data.data(), image_block.data(), 1, STB_DXT_HIGHQUAL);
+                std::copy(block_data.begin(), block_data.end(), std::back_inserter(result));
+            }
+        }
+        return result;
+    }
+
+    std::vector<uint8_t> create_compressed_texture_data(const std::string& image_path, texture_compression_type type)
+    {
+        ien::image source_image(image_path);
+
+        CRITICAL_CHECK(source_image.width() % 4 == 0);
+        CRITICAL_CHECK(source_image.height() % 4 == 0);
+
+        switch (type)
+        {
+        case texture_compression_type::DXT1_BC1:
+            return compress_dxt1(source_image);
+        case texture_compression_type::DXT5_BC3:
+            return compress_dxt5(source_image);
+        }
+    }
+} // namespace cathedral::engine
