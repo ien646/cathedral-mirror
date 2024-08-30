fdc6cac6d9f1d327dba5a83623f6cb9700494495
ien <ien@amsostudios.com>
2024-08-30 00:00:00 +0000

    better docking

---
+++ lib/editor/include/cathedral/editor/common/dock_title.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <QLabel>
+
+namespace cathedral::editor
+{
+    class dock_title : public QLabel
+    {
+    public:
+        dock_title(QString text, QWidget* parent = nullptr);
+    };
+}
