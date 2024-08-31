8577cfc3d853f5f3870e78954e7bb959a6bfc902
ien <ien@amsostudios.com>
2024-08-31 00:00:00 +0000

    shader editor syntax highlighter

---
+++ lib/editor/include/cathedral/editor/asset_managers/shader_syntax_highlighter.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <QSyntaxHighlighter>
+
+#include <vector>
+
+namespace cathedral::editor
+{
+    class shader_syntax_highlighter : public QSyntaxHighlighter
+    {
+    public:
+        shader_syntax_highlighter(QTextDocument* parent);
+
+    protected:
+        void highlightBlock(const QString& text) override;
+
+    private:
+        struct FormatRuleGroup
+        {
+            std::vector<QRegularExpression> patterns;
+            QTextCharFormat format;
+        };
+        std::vector<FormatRuleGroup> _rules;
+    };
+} // namespace cathedral::editor
