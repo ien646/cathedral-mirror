d54f5618c4c137f29463d1c2ba8eb734ea27a710
ien <ien@amsostudios.com>
2024-09-03 00:00:00 +0000

    mvp mesh_manager

--- lib/editor/src/common/path_selector.cpp
+++ lib/editor/src/common/path_selector.cpp
@@ -5,6 +5,8 @@
 #include <QLabel>
 #include <QLineEdit>
 #include <QPushButton>
+#include <QStyle>
+#include <QStyleOption>
 
 namespace cathedral::editor
 {
@@ -12,16 +14,18 @@ namespace cathedral::editor
         : QWidget(parent)
         , _mode(mode)
     {
-        _main_layout = new QHBoxLayout(this);
-        _line_edit = new QLineEdit(this);
-        _browse_button = new QPushButton(this);
+        _main_layout = new QHBoxLayout;
+        _line_edit = new QLineEdit;
+        _browse_button = new QPushButton;
 
         _line_edit->setReadOnly(true);
-        _browse_button->setText("...");
+
+        _browse_button->setText("Browse");
+        _browse_button->setFixedWidth(_browse_button->fontMetrics().horizontalAdvance("Browse") + 8);
 
         if (!label.isEmpty())
         {
-            _label = new QLabel(this);
+            _label = new QLabel;
             _label->setText(label);
         }
 
@@ -31,6 +35,9 @@ namespace cathedral::editor
         }
         _main_layout->addWidget(_line_edit, 1);
         _main_layout->addWidget(_browse_button, 0, Qt::AlignRight);
+        _main_layout->setContentsMargins(0, 0, 0, 0);
+
+        setLayout(_main_layout);
 
         connect(_browse_button, &QPushButton::clicked, this, [this] { handle_browse_click(); });
     }
@@ -49,10 +56,12 @@ namespace cathedral::editor
     {
         const std::string dialog_text = _mode == path_selector_mode::DIRECTORY ? "Select a directory" : "Select a file";
         QFileDialog dialog(this);
+        dialog.setFileMode(_mode == path_selector_mode::DIRECTORY ? QFileDialog::FileMode::Directory : QFileDialog::FileMode::ExistingFile);
         dialog.setWindowTitle(QString::fromStdString(dialog_text));
         if (dialog.exec())
         {
             const auto selected = dialog.selectedFiles();
+            _line_edit->setText(selected.at(0));
             if (!selected.empty())
             {
                 emit paths_selected(selected);
