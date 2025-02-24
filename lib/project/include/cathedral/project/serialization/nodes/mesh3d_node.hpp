8397fcafa9a02dcd701e0303dd6a0e5359723abf
ien <ien@amsostudios.com>
2025-02-24 00:00:00 +0000

    fixes + wip scene serialization

--- lib/project/include/cathedral/project/serialization/nodes/mesh3d_node.hpp
+++ lib/project/include/cathedral/project/serialization/nodes/mesh3d_node.hpp
@@ -9,12 +9,18 @@ namespace cereal
     template <typename Archive>
     void CEREAL_SAVE_FUNCTION_NAME(Archive& ar, const cathedral::engine::mesh3d_node& node)
     {
-        ar(cereal::make_nvp("not implemented", ""));
+        ar(cereal::make_nvp("name", node.name()),
+           cereal::make_nvp("type", std::string{ node.typestr() }),
+           cereal::make_nvp("enabled", node.enabled()),
+           cereal::make_nvp("children", node.children()),
+           cereal::make_nvp("transform", node.get_local_transform()),
+           cereal::make_nvp("mesh_name", node.mesh_name()));
     }
 
     template <typename Archive>
     void CEREAL_LOAD_FUNCTION_NAME(Archive& ar, cathedral::engine::mesh3d_node& node)
     {
+        CRITICAL_ERROR("Not implemented");
     }
 } // namespace cereal
 
