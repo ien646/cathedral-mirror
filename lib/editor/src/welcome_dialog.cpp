e6e49c1521e379607c1f23c47376140a98dcd825
ien <ien@amsostudios.com>
2025-02-25 00:00:00 +0000

    new/open project welcome dialog

--- lib/editor/src/welcome_dialog.cpp
+++ lib/editor/src/welcome_dialog.cpp
@@ -20,34 +20,50 @@ namespace cathedral::editor
         connect(_ui->pushButton_NewProject, &QPushButton::clicked, this, [this] {
             QFileDialog file_dialog(this, "Select directory to create a new project");
             file_dialog.setFileMode(QFileDialog::FileMode::Directory);
-            if (file_dialog.exec() != 0)
+            if (file_dialog.exec() == QDialog::Rejected)
             {
-                const auto& selected_path = file_dialog.selectedFiles().at(0).toStdString();
-                QDir dir(QString::fromStdString(selected_path));
-                if (dir.isEmpty())
-                {
-                    return;
-                }
-                if (!show_confirm_dialog("Selected directory is not empty, continue?", this))
-                {
-                    return;
-                }
-
-                auto* name_dialog = new text_input_dialog(this, "Give your new project a name", "Name: ", false);
-                if (!name_dialog->exec())
-                {
-                    return;
-                }
-
-                const auto& name = name_dialog->result().toStdString();
-
-                _project = std::make_shared<project::project>(project::project::create(selected_path, name));
-                accept();
+                return;
             }
+
+            const auto& selected_path = file_dialog.selectedFiles().at(0).toStdString();
+            QDir dir(QString::fromStdString(selected_path));
+            if (!dir.isEmpty() && !show_confirm_dialog("Selected directory is not empty, continue?", this))
+            {
+                return;
+            }
+
+            auto* name_dialog = new text_input_dialog(this, "Give your new project a name", "Name: ", false);
+            if (!name_dialog->exec())
+            {
+                return;
+            }
+
+            const auto& name = name_dialog->result().toStdString();
+
+            _project = std::make_shared<project::project>(project::project::create(selected_path, name));
+            accept();
         });
 
         connect(_ui->pushButton_OpenProject, &QPushButton::clicked, this, [this] {
-            show_error_message("Not implemented");
+            QFileDialog file_dialog(this, "Select directory containing an existing project");
+            file_dialog.setFileMode(QFileDialog::FileMode::Directory);
+            if (file_dialog.exec() == QDialog::Rejected)
+            {
+                return;
+            }
+
+            const auto& project_path = file_dialog.selectedFiles().at(0).toStdString();
+            const auto& project_file_path = std::filesystem::path(project_path) / ".cathedral";
+            if (!std::filesystem::exists(project_file_path))
+            {
+                show_error_message("Project file not found!");
+                return;
+            }
+
+            _project = std::make_shared<project::project>();
+            _project->load_project(project_path);
+
+            accept();
         });
     }
 
