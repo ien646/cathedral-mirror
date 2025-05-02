ce6827ab7a0db6040ee072a367a041828f8dc125
ien <ien@amsostudios.com>
2025-05-02 00:00:00 +0000

    padding macros

--- lib/core/include/cathedral/bits/macros.hpp
+++ lib/core/include/cathedral/bits/macros.hpp
@@ -13,3 +13,11 @@
 #define CATHEDRAL_DEFAULT_MOVABLE(type) /*NOLINT */                                                                         \
     type(type&&) = default;                                                                                                 \
     type& operator=(type&&) = default
+
+#define CATHEDRAL_CONCAT(a, b) a ## b
+#define CATHEDRAL_UNIQUE_SUFFIX(x, id) CATHEDRAL_CONCAT(x, id)
+
+#define CATHEDRAL_PADDING_8  const uint8_t  CATHEDRAL_UNIQUE_SUFFIX(_cathedral_padding_, __COUNTER__) = 0xADu
+#define CATHEDRAL_PADDING_16 const uint16_t CATHEDRAL_UNIQUE_SUFFIX(_cathedral_padding_, __COUNTER__) = 0xADADu
+#define CATHEDRAL_PADDING_32 const uint32_t CATHEDRAL_UNIQUE_SUFFIX(_cathedral_padding_, __COUNTER__) = 0xADADADADu
+#define CATHEDRAL_PADDING_64 const uint64_t CATHEDRAL_UNIQUE_SUFFIX(_cathedral_padding_, __COUNTER__) = 0xADADADADADADADADu
