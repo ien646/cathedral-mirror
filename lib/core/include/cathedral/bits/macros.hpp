c75af90e11979d603b5974b5372e4e2547ee8021
ien <ien@amsostudios.com>
2025-05-02 00:00:00 +0000

    compile definable byte for padding macros

--- lib/core/include/cathedral/bits/macros.hpp
+++ lib/core/include/cathedral/bits/macros.hpp
@@ -14,10 +14,25 @@
     type(type&&) = default;                                                                                                 \
     type& operator=(type&&) = default
 
-#define CATHEDRAL_CONCAT(a, b) a ## b
+#define CATHEDRAL_CONCAT(a, b) a##b
 #define CATHEDRAL_UNIQUE_SUFFIX(x, id) CATHEDRAL_CONCAT(x, id)
 
-#define CATHEDRAL_PADDING_8  const uint8_t  CATHEDRAL_UNIQUE_SUFFIX(_cathedral_padding_, __COUNTER__) = 0xADu
-#define CATHEDRAL_PADDING_16 const uint16_t CATHEDRAL_UNIQUE_SUFFIX(_cathedral_padding_, __COUNTER__) = 0xADADu
-#define CATHEDRAL_PADDING_32 const uint32_t CATHEDRAL_UNIQUE_SUFFIX(_cathedral_padding_, __COUNTER__) = 0xADADADADu
-#define CATHEDRAL_PADDING_64 const uint64_t CATHEDRAL_UNIQUE_SUFFIX(_cathedral_padding_, __COUNTER__) = 0xADADADADADADADADu
+#ifndef CATHEDRAL_PADDING_BYTE
+    #define CATHEDRAL_PADDING_BYTE 0xADull
+#endif
+
+#define CATHEDRAL_PADDING_8 const uint8_t CATHEDRAL_UNIQUE_SUFFIX(_cathedral_padding_, __COUNTER__) = CATHEDRAL_PADDING_BYTE
+
+#define CATHEDRAL_PADDING_16                                                                                                \
+    const uint16_t CATHEDRAL_UNIQUE_SUFFIX(_cathedral_padding_, __COUNTER__) = CATHEDRAL_PADDING_BYTE |                     \
+                                                                               (CATHEDRAL_PADDING_BYTE << 8)
+#define CATHEDRAL_PADDING_32                                                                                                \
+    const uint32_t CATHEDRAL_UNIQUE_SUFFIX(_cathedral_padding_, __COUNTER__) =                                              \
+        CATHEDRAL_PADDING_BYTE | (CATHEDRAL_PADDING_BYTE << 8) | (CATHEDRAL_PADDING_BYTE << 16) |                           \
+        (CATHEDRAL_PADDING_BYTE << 24)
+
+#define CATHEDRAL_PADDING_64                                                                                                \
+    const uint64_t CATHEDRAL_UNIQUE_SUFFIX(_cathedral_padding_, __COUNTER__) =                                              \
+        CATHEDRAL_PADDING_BYTE | (CATHEDRAL_PADDING_BYTE << 8) | (CATHEDRAL_PADDING_BYTE << 16) |                           \
+        (CATHEDRAL_PADDING_BYTE << 24) | (CATHEDRAL_PADDING_BYTE << 32) | (CATHEDRAL_PADDING_BYTE << 40) |                  \
+        (CATHEDRAL_PADDING_BYTE << 48) | (CATHEDRAL_PADDING_BYTE << 56)
