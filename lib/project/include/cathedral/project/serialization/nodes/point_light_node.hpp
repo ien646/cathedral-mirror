7d15611be32df5ff45feef4246a64ca4754dfc6e
ien646 <1340246+ien646@users.noreply.github.com>
2025-05-08 00:00:00 +0000

    missing point light node serialization

--- lib/project/include/cathedral/project/serialization/nodes/point_light_node.hpp
+++ lib/project/include/cathedral/project/serialization/nodes/point_light_node.hpp
@@ -2,17 +2,33 @@
 
 #include <cathedral/engine/nodes/point_light_node.hpp>
 
-#include <cereal/cereal.hpp>
+#include <cereal/types/memory.hpp>
+#include <cereal/types/polymorphic.hpp>
+#include <cereal/types/string.hpp>
 
 namespace cereal
 {
     template <typename Archive>
+    void CEREAL_SERIALIZE_FUNCTION_NAME(Archive& ar, cathedral::engine::point_light_data& pldata)
+    {
+        ar(pldata.enabled, pldata.position, pldata.intensity, pldata.range, pldata.falloff_coefficient, pldata.color);
+    }
+
+    template <typename Archive>
     void CEREAL_SAVE_FUNCTION_NAME(Archive& ar, const cathedral::engine::point_light_node& node)
     {
+        // Filter out editor nodes
+        auto child_nodes_range = node.children() |
+                                 std::views::filter([](const std::shared_ptr<cathedral::engine::scene_node>& child) {
+                                     return !child->name().starts_with("__");
+                                 });
+        const std::vector<std::shared_ptr<cathedral::engine::scene_node>> children = { child_nodes_range.begin(),
+                                                                                       child_nodes_range.end() };
+
         ar(make_nvp("name", node.name()),
            make_nvp("type", std::string{ node.typestr() }),
            make_nvp("enabled", node.enabled()),
-           make_nvp("children", node.children()),
+           make_nvp("children", children),
            make_nvp("transform", node.get_local_transform()),
            make_nvp("point_light_data", node.data()));
     }
@@ -41,3 +57,6 @@ namespace cereal
         node.set_falloff_coefficient(pl_data.falloff_coefficient);
     }
 } // namespace cereal
+
+CEREAL_REGISTER_TYPE(cathedral::engine::point_light_node);
+CEREAL_REGISTER_POLYMORPHIC_RELATION(cathedral::engine::scene_node, cathedral::engine::point_light_node);
--- lib/project/include/cathedral/project/serialization/scene.hpp
+++ lib/project/include/cathedral/project/serialization/scene.hpp
@@ -4,6 +4,7 @@
 #include <cathedral/project/serialization/nodes/camera3d_node.hpp>
 #include <cathedral/project/serialization/nodes/mesh3d_node.hpp>
 #include <cathedral/project/serialization/nodes/node.hpp>
+#include <cathedral/project/serialization/nodes/point_light_node.hpp>
 
 #include <cathedral/engine/scene.hpp>
 
@@ -15,7 +16,15 @@ namespace cereal
     template <typename Archive>
     void CEREAL_SAVE_FUNCTION_NAME(Archive& ar, const cathedral::engine::scene& scene)
     {
-        ar(cereal::make_nvp("root_nodes", scene.root_nodes()));
+        // Filter out editor nodes
+        auto root_nodes_range = scene.root_nodes() |
+                                std::views::filter([](const std::shared_ptr<cathedral::engine::scene_node>& child) {
+                                    return !child->name().starts_with("__");
+                                });
+        const std::vector<std::shared_ptr<cathedral::engine::scene_node>> root_nodes = { root_nodes_range.begin(),
+                                                                                         root_nodes_range.end() };
+
+        ar(cereal::make_nvp("root_nodes", root_nodes));
     }
 
     template <typename Archive>
