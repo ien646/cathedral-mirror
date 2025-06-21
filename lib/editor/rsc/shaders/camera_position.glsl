1a7e8368585cdb352b197dfdb5d45022fcc44e3f
ien646 <1340246+ien646@users.noreply.github.com>
2025-06-21 00:00:00 +0000

    some more shader templates

---
+++ lib/editor/rsc/shaders/camera_position.glsl
@@ -0,0 +1,9 @@
+vec3 get_camera2d_position()
+{
+    return vec3(VIEW_2D[3][0], VIEW_2D[3][1], VIEW_2D[3][2]);
+}
+
+vec3 get_camera3d_position()
+{
+    return vec3(VIEW_3D[3][0], VIEW_3D[3][1], VIEW_3D[3][2]);
+}
