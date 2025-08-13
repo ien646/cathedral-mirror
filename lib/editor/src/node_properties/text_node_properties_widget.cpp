48dc9f21f2385ee2ffe6d68a6a33c0afc970d7d6
ien646 <1340246+ien646@users.noreply.github.com>
2025-08-13 00:00:00 +0000

    WIP text node

--- lib/editor/src/node_properties/text_node_properties_widget.cpp
+++ lib/editor/src/node_properties/text_node_properties_widget.cpp
@@ -1,8 +1,11 @@
 #include "cathedral/editor/common/vertical_separator.hpp"
 #include "cathedral/editor/node_properties/font_selector.hpp"
+#include "cathedral/editor/node_properties/material_selector.hpp"
+#include "cathedral/editor/utils.hpp"
 #include "cathedral/engine/scene.hpp"
 
 #include <QLabel>
+#include <QLineEdit>
 #include <QVBoxLayout>
 #include <cathedral/editor/node_properties/text_node_properties_widget.hpp>
 
@@ -27,8 +30,44 @@ namespace cathedral::editor
         auto* font_selector = new editor::font_selector(_project, scene, this, "no font");
         _main_layout->addWidget(font_selector);
 
-        connect(font_selector, &font_selector::font_selected, this, [this, text_node](const std::shared_ptr<project::font_asset>& asset) {
-            text_node->set_font_name(asset->name());
+        connect(
+            font_selector,
+            &font_selector::font_selected,
+            this,
+            [text_node, font_selector](const std::shared_ptr<project::font_asset>& asset) {
+                text_node->set_font_name(asset->name());
+                font_selector->set_text(QSTR(asset->name()));
+            });
+
+        _main_layout->addWidget(new QLabel("Material"));
+        _main_layout->addWidget(new vertical_separator(this));
+
+        auto* material_selector = new editor::material_selector(_project, scene, this, "no material");
+        _main_layout->addWidget(material_selector);
+
+        connect(
+            material_selector,
+            &material_selector::material_selected,
+            this,
+            [text_node, material_selector](const std::shared_ptr<project::material_asset>& asset) {
+                text_node->set_material(asset->name());
+                material_selector->set_text(QSTR(asset->name()));
+            });
+
+        _main_layout->addWidget(new QLabel("Text"));
+        _main_layout->addWidget(new vertical_separator(this));
+
+        auto* text_layout = new QHBoxLayout;
+        _main_layout->addLayout(text_layout);
+
+        text_layout->addWidget(new QLabel("Text: "));
+
+        auto* text_edit = new QLineEdit;
+        text_edit->setText("Lorem Ipsum");
+        text_layout->addWidget(text_edit, 1);
+
+        connect(text_edit, &QLineEdit::textChanged, this, [text_node](const QString& text) {
+            text_node->set_text(text.toStdU32String());
         });
 
         _main_layout->addStretch(1);
