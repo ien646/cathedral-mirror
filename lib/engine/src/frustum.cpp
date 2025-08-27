4bd99617336dfaaebeea6ecdcaadf846e1d64784
ien646 <1340246+ien646@users.noreply.github.com>
2025-08-27 00:00:00 +0000

    roomba

--- lib/engine/src/frustum.cpp
+++ lib/engine/src/frustum.cpp
@@ -9,63 +9,66 @@
 
 namespace cathedral::engine
 {
-    frustum_planes get_frustum_from_viewproj_matrix(const glm::mat4& vp_matrix)
+    namespace
     {
-        frustum_planes frustum;
+        frustum_planes get_frustum_from_viewproj_matrix(const glm::mat4& vp_matrix)
+        {
+            frustum_planes frustum;
 
-        // Left
-        frustum.left = glm::vec4(
-            vp_matrix[0][3] + vp_matrix[0][0],
-            vp_matrix[1][3] + vp_matrix[1][0],
-            vp_matrix[2][3] + vp_matrix[2][0],
-            vp_matrix[3][3] + vp_matrix[3][0]);
+            // Left
+            frustum.left = glm::vec4(
+                vp_matrix[0][3] + vp_matrix[0][0],
+                vp_matrix[1][3] + vp_matrix[1][0],
+                vp_matrix[2][3] + vp_matrix[2][0],
+                vp_matrix[3][3] + vp_matrix[3][0]);
 
-        // Right
-        frustum.right = glm::vec4(
-            vp_matrix[0][3] - vp_matrix[0][0],
-            vp_matrix[1][3] - vp_matrix[1][0],
-            vp_matrix[2][3] - vp_matrix[2][0],
-            vp_matrix[3][3] - vp_matrix[3][0]);
+            // Right
+            frustum.right = glm::vec4(
+                vp_matrix[0][3] - vp_matrix[0][0],
+                vp_matrix[1][3] - vp_matrix[1][0],
+                vp_matrix[2][3] - vp_matrix[2][0],
+                vp_matrix[3][3] - vp_matrix[3][0]);
 
-        // Bottom
-        frustum.bottom = glm::vec4(
-            vp_matrix[0][3] + vp_matrix[0][1],
-            vp_matrix[1][3] + vp_matrix[1][1],
-            vp_matrix[2][3] + vp_matrix[2][1],
-            vp_matrix[3][3] + vp_matrix[3][1]);
+            // Bottom
+            frustum.bottom = glm::vec4(
+                vp_matrix[0][3] + vp_matrix[0][1],
+                vp_matrix[1][3] + vp_matrix[1][1],
+                vp_matrix[2][3] + vp_matrix[2][1],
+                vp_matrix[3][3] + vp_matrix[3][1]);
 
-        // Top
-        frustum.top = glm::vec4(
-            vp_matrix[0][3] - vp_matrix[0][1],
-            vp_matrix[1][3] - vp_matrix[1][1],
-            vp_matrix[2][3] - vp_matrix[2][1],
-            vp_matrix[3][3] - vp_matrix[3][1]);
+            // Top
+            frustum.top = glm::vec4(
+                vp_matrix[0][3] - vp_matrix[0][1],
+                vp_matrix[1][3] - vp_matrix[1][1],
+                vp_matrix[2][3] - vp_matrix[2][1],
+                vp_matrix[3][3] - vp_matrix[3][1]);
 
-        // Near
-        frustum.near = glm::vec4(
-            vp_matrix[0][3] + vp_matrix[0][2],
-            vp_matrix[1][3] + vp_matrix[1][2],
-            vp_matrix[2][3] + vp_matrix[2][2],
-            vp_matrix[3][3] + vp_matrix[3][2]);
+            // Near
+            frustum.near = glm::vec4(
+                vp_matrix[0][3] + vp_matrix[0][2],
+                vp_matrix[1][3] + vp_matrix[1][2],
+                vp_matrix[2][3] + vp_matrix[2][2],
+                vp_matrix[3][3] + vp_matrix[3][2]);
 
-        // Far
-        frustum.far = glm::vec4(
-            vp_matrix[0][3] - vp_matrix[0][2],
-            vp_matrix[1][3] - vp_matrix[1][2],
-            vp_matrix[2][3] - vp_matrix[2][2],
-            vp_matrix[3][3] - vp_matrix[3][2]);
+            // Far
+            frustum.far = glm::vec4(
+                vp_matrix[0][3] - vp_matrix[0][2],
+                vp_matrix[1][3] - vp_matrix[1][2],
+                vp_matrix[2][3] - vp_matrix[2][2],
+                vp_matrix[3][3] - vp_matrix[3][2]);
 
-        const auto normalize_plane = [](plane& plane) { plane.abcd /= glm::length(glm::vec3(plane.abcd)); };
+            const auto normalize_plane = [](plane& plane) { plane.abcd /= glm::length(glm::vec3(plane.abcd)); };
 
-        normalize_plane(frustum.near);
-        normalize_plane(frustum.far);
-        normalize_plane(frustum.left);
-        normalize_plane(frustum.right);
-        normalize_plane(frustum.bottom);
-        normalize_plane(frustum.top);
+            normalize_plane(frustum.near);
+            normalize_plane(frustum.far);
+            normalize_plane(frustum.left);
+            normalize_plane(frustum.right);
+            normalize_plane(frustum.bottom);
+            normalize_plane(frustum.top);
 
-        return frustum;
-    }
+            return frustum;
+        }
+    } // namespace
 
     frustum_planes get_frustum_from_camera(const camera& camera)
     {
