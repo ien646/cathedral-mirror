5546c5ecd99068cc3e7d25f41719327408fdd417
ien646 <1340246+ien646@users.noreply.github.com>
2025-11-22 00:00:00 +0000

    fix constexpr-ness on MSVC

--- lib/engine/include/cathedral/engine/node_type.hpp
+++ lib/engine/include/cathedral/engine/node_type.hpp
@@ -10,24 +10,33 @@ namespace cathedral::engine
     {
     public:
         using id_type = uint64_t;
+        constexpr static auto STR_SIZE = sizeof(id_type);
+        using chars_t = std::span<const char, STR_SIZE + 1>;
 
-        constexpr static node_type from_chars(const std::string_view sv)
+        constexpr static node_type from_chars(const chars_t chars)
         {
-            CRITICAL_CHECK(sv.size() == 8, "node type id requires 8 chars exactly");
             node_type result{};
-            result._id = *reinterpret_cast<const id_type*>(sv.data());
+            result._id = (static_cast<uint64_t>(chars[0]) << 56)
+                         | (static_cast<uint64_t>(chars[1]) << 48)
+                         | (static_cast<uint64_t>(chars[2]) << 40)
+                         | (static_cast<uint64_t>(chars[3]) << 32)
+                         | (static_cast<uint64_t>(chars[4]) << 24)
+                         | (static_cast<uint64_t>(chars[5]) << 16)
+                         | (static_cast<uint64_t>(chars[6]) << 8)
+                         | static_cast<uint64_t>(chars[7]);
             return result;
         }
 
-        bool operator==(const node_type& rhs) const { return _id == rhs._id; }
+        constexpr bool operator==(const node_type& rhs) const { return _id == rhs._id; }
 
-        constexpr std::string_view string_view() const { return std::string_view{ reinterpret_cast<const char*>(&_id), 8 }; }
+        constexpr std::string_view string_view() const
+        {
+            return std::string_view{ reinterpret_cast<const char*>(&_id), STR_SIZE };
+        }
 
-        void replace(const std::string_view sv) { *this = from_chars(sv); }
+        constexpr void replace(const chars_t sv) { *this = from_chars(sv); }
 
-        id_type id() const { return _id; }
-
-        friend struct std::hash<node_type>;
+        constexpr id_type id() const { return _id; }
 
     private:
         id_type _id;
